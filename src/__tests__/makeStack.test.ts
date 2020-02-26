a3a424d4861c2078a1e49059215a6866cd4a06d5
elia_camposilvan <elia.camposilvan@rawfish.it>
2020-02-27 00:31:44 +0100

    stack structure added

---
+++ src/__tests__/makeStack.test.ts
@@ -0,0 +1,55 @@
+import { makeStack } from "../makeStack"
+import { EmptyStackError } from "../errors"
+
+describe("with an empty stack...", () => {
+  it("should have no top value", () => {
+    const stack = makeStack()
+    expect(stack.peek()).toBeUndefined()
+  })
+
+  it("should be empty", () => {
+    const stack = makeStack()
+    expect(stack.isEmpty()).toBe(true)
+  })
+
+  it("should throw during pop operation", () => {
+    const stack = makeStack()
+    expect(stack.pop).toThrowError(EmptyStackError)
+  })
+})
+
+describe("when adding single element...", () => {
+  it("should be retrievable through peek", () => {
+    const stack = makeStack()
+    stack.push(1)
+    expect(stack.peek()).toBe(1)
+  })
+
+  it("should be empty after element removal", () => {
+    const stack = makeStack()
+    stack.push(1)
+    expect(stack.pop()).toBe(1)
+    expect(stack.isEmpty()).toBe(true)
+  })
+})
+
+describe("when adding multiple values", () => {
+  it("should be in LIFO order", () => {
+    const stack = makeStack()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    expect(stack.pop()).toBe(3)
+    expect(stack.pop()).toBe(2)
+    expect(stack.pop()).toBe(1)
+  })
+})
+
+describe("when defined with initial elements...", () => {
+  it("should be in LIFO order", () => {
+    const stack = makeStack(1, 2, 3)
+    expect(stack.pop()).toBe(3)
+    expect(stack.pop()).toBe(2)
+    expect(stack.pop()).toBe(1)
+  })
+})
