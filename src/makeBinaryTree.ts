bd16b633cb07569d1b3349dad9842532d15d0dcf
elia_camposilvan <elia.camposilvan@rawfish.it>
2020-03-14 19:38:19 +0100

    binary tree traversal fns added

--- src/makeBinaryTree.ts
+++ src/makeBinaryTree.ts
@@ -1,6 +1,9 @@
 export type BinaryTreeNode<T> = {
   value: T
   getDepth: () => number
+  inOrderVertices: () => Generator<T, void, unknown>
+  preOrderVertices: () => Generator<T, void, unknown>
+  postOrderVertices: () => Generator<T, void, unknown>
   setLeft: (node: BinaryTreeNode<T> | null) => void
   setRight: (node: BinaryTreeNode<T> | null) => void
   getLeft: () => BinaryTreeNode<T> | null
@@ -18,24 +21,92 @@ export function makeBinaryTreeNode<T>(nodeValue: T): BinaryTreeNode<T> {
   let left: BinaryTreeNode<T> | null = null
   let right: BinaryTreeNode<T> | null = null
 
+  /**
+   * Retrieves the maximum depth of the tree.
+   */
   function getDepth() {
     return Math.max(left?.getDepth() ?? 0, right?.getDepth() ?? 0) + 1
   }
 
+  /**
+   * Sets the **left** vertex.
+   */
   function setLeft(node: BinaryTreeNode<T> | null) {
     left = node
   }
 
+  /**
+   * Sets the **right** vertex.
+   */
   function setRight(node: BinaryTreeNode<T> | null) {
     right = node
   }
 
+  /**
+   * Retrieves the **left** vertex.
+   */
   function getLeft() {
     return left
   }
 
+  /**
+   * Retrieves the **right** vertex.
+   */
   function getRight() {
     return right
+  }
+
+  /**
+   * Retrieves an Iterable that loads the node **left** branch
+   * vertices values, then the **current** vertex value, and then
+   * the **right** branch vertices values
+   * Syntax:
+   * ```
+   * for (const value of node.inOrderVertices()) {
+   *   ...
+   * }
+   * ```
+   */
+  function* inOrderVertices() {
+    left?.inOrderVertices()
+    yield value
+    right?.inOrderVertices()
+  }
+
+  /**
+   * Retrieves an Iterable that loads the **current** vertex value,
+   * then the node **left** branch vertices values, and then the
+   *  **right** branch vertices values.
+   * Root vertex value is always the first one to be loaded
+   * Syntax:
+   * ```
+   * for (const value of node.preOrderVertices()) {
+   *   ...
+   * }
+   * ```
+   */
+  function* preOrderVertices() {
+    yield value
+    left?.preOrderVertices()
+    right?.preOrderVertices()
+  }
+
+  /**
+   * Retrieves an Iterable that loads the node **left** branch
+   * vertices values, then the **current** vertex value, and then
+   * the **right** branch vertices values
+   * Root vertex value is always the last one to be loaded
+   * Syntax:
+   * ```
+   * for (const value of node.postOrderVertices()) {
+   *   ...
+   * }
+   * ```
+   */
+  function* postOrderVertices() {
+    left?.postOrderVertices()
+    right?.postOrderVertices()
+    yield value
   }
 
   return {
@@ -45,6 +116,9 @@ export function makeBinaryTreeNode<T>(nodeValue: T): BinaryTreeNode<T> {
     setRight,
     getLeft,
     getRight,
+    inOrderVertices,
+    preOrderVertices,
+    postOrderVertices,
   }
 }
 
